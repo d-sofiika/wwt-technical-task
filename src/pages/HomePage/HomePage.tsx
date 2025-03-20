d5a467233a00b036482b44be850b2524a9a2aebf
d-sofiika <soniadrozd2709@gmail.com>
2025-03-21 00:57:29 +0200

    Update HomePage.tsx

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 
 import { Box, Button, Flex, Text } from '@chakra-ui/react'
